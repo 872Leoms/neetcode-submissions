d3cfe03f2bebda1cb0491810cd6f4a9e4310825d
872Leoms <lololele595@icloud.com>
2026-08-02 06:26:53 +0300

    Add: design-word-search-data-structure - submission-9

---
+++ Data Structures & Algorithms/design-word-search-data-structure/submission-9.py
@@ -0,0 +1,40 @@
+class Node:
+    def __init__(self,val = None,end = False):
+        self.val = val
+        self.end = end 
+        self.children = {}
+class WordDictionary:
+    def __init__(self):
+        self.root = Node("root")
+
+    def addWord(self, word: str) -> None:
+        head = self.root
+        for i in range(len(word)):
+            val = word[i]
+            itend = True if i == len(word) - 1 else False
+            node = Node(val,itend)
+            if val not in head.children:
+                head.children[val] = node
+            if itend:
+                head.children[val].end = itend
+            head = head.children[val]
+
+    def search(self, word: str) -> bool:
+        def rec(node,ind):
+            if ind == len(word):
+                return node.end
+            if word[ind] in node.children:
+                print("in",word[ind],node.val)
+                return rec(node.children[word[ind]],ind + 1)
+            else:
+                if word[ind] == ".":
+                    for child in node.children:
+                         temp =  rec(node.children[child], ind + 1)
+                         if temp:
+                            return True
+                    return False
+                else:
+                    return False
+        return rec(self.root,0)
+            
+    
