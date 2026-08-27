768ea417bdd6a011a895e45f3a65a1e30d866118
872Leoms <lololele595@icloud.com>
2026-08-28 01:27:39 +0300

    Add: design-twitter-feed - submission-17

---
+++ Data Structures & Algorithms/design-twitter-feed/submission-17.py
@@ -0,0 +1,54 @@
+import heapq
+class account:
+    def __init__(self):
+        self.mytweet = []
+        self.folowing = set()
+class Twitter:
+    def __init__(self):
+        self.count = 0
+        self.users = {}
+    def checkuser(self,idd):
+        if idd not in self.users:
+            self.users[idd] = account()
+        return self.users[idd]
+    def postTweet(self, userId: int, tweetId: int) -> None:
+        account = self.checkuser(userId)
+        self.count += 1
+        heapq.heappush(account.mytweet,(-self.count,tweetId))
+    def getNewsFeed(self, userId: int) -> List[int]:
+        user = self.checkuser(userId)
+        feed = []
+        following = user.folowing.copy() | {userId}
+        for i in following:
+            fuser = self.checkuser(i)
+            for j in fuser.mytweet:
+                if len(feed) < 10:
+                    a,b = j
+                    heapq.heappush(feed,(-a,b))
+                elif feed[0][0] < -j[0]:
+                    a,b = j
+                    heapq.heappop(feed)
+                    heapq.heappush(feed,(-a,b))
+        res = []
+        while feed:
+            f = heapq.heappop(feed)
+            res.append(f[1])
+        return res[::-1]
+
+    def follow(self, followerId: int, followeeId: int) -> None:
+        if followerId == followeeId:
+            return
+        folloid = self.checkuser(followerId)
+        follweeid = self.checkuser(followeeId)
+        folloid.folowing.add(followeeId)
+
+    def unfollow(self, followerId: int, followeeId: int) -> None:
+        if followerId == followeeId:
+            return
+        folloid = self.checkuser(followerId)
+        if followeeId not in folloid.folowing:
+            return 
+        follweeid = self.checkuser(followeeId)
+        folloid.folowing.remove(followeeId)
+
+        
